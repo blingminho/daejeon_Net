30e25cad7f91b7fdd3aec5f59a9fadf8cb4527ae
blingminho <tkdjun67@naver.com>
2018-03-23 14:52:11 +0900

    파일을 두개 전송하는 방법으로 변경

--- src/tcp/TcpClient3.java
+++ src/tcp/TcpClient3.java
@@ -12,11 +12,6 @@ import java.net.UnknownHostException;
 public class TcpClient3 {
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		String serverIp = "192.168.0.33";
-		System.out.println("서버에 연결을 합니다");
-		
-		Socket socket = new Socket(serverIp, 7777);
-		
-		System.out.println("서버와 연결되었습니다.");
 		
 		
 //		Scanner sc = new Scanner(System.in);
@@ -24,25 +19,36 @@ public class TcpClient3 {
 //		String filePath = sc.nextLine();
 		
 		String filePath = "d:/javaIO/Tulips.jpg";
+		String filePath1 = "d:/javaIO/Desert.jpg";
 		
+		String[] filepath = {filePath, filePath1};
 		
-		File file = new File(filePath);
-		FileInputStream fis = new FileInputStream(file);
-		byte[] buffer = new byte[2048];
-		int readBytes;
-		
-		OutputStream os = socket.getOutputStream();
-		DataOutputStream dos = new DataOutputStream(os);
-		
-		while ((readBytes = fis.read(buffer)) != -1) {
-			dos.write(buffer, 0, readBytes);
+		for (String filePath2 : filepath) {
+			System.out.println("서버에 연결을 합니다");
+			
+			Socket socket = new Socket(serverIp, 7777);
+			
+			System.out.println("서버와 연결되었습니다.");
+
+			File file = new File(filePath2);
+			FileInputStream fis = new FileInputStream(file);
+			byte[] buffer = new byte[2048];
+			int readBytes;
+			
+			OutputStream os = socket.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(os);
+			
+			while ((readBytes = fis.read(buffer)) != -1) {
+				dos.write(buffer, 0, readBytes);
+			}
+			
+			System.out.println("서버종료");
+			fis.close();
+			dos.close();
+			socket.close();			
 		}
 		
-		System.out.println("서버종료");
 		
-		fis.close();
-		dos.close();
-		socket.close();
 		
 	}
 }
--- src/tcp/TcpServer3.java
+++ src/tcp/TcpServer3.java
@@ -18,24 +18,31 @@ public class TcpServer3 {
 			serverSocket = new ServerSocket(7777);
 			System.out.println("서버가 접속을 기다리고 있습니다.");
 			
-			Socket socket = serverSocket.accept();
+			String filePath = "d:/javaIO/연습용/Tulips.jpg";
+			String filePath1 = "d:/javaIO/연습용/Desert.jpg";
 			
-			InputStream is = socket.getInputStream();
-			DataInputStream dis = new DataInputStream(is);
+			String[] filepath = {filePath, filePath1};
 			
-			File file = new File("d:/javaIO/연습용/Tulips.jpg");
-			fos = new FileOutputStream(file);
-			
-			int readBytes = 0;
-			byte[] buffer = new byte[2048];
-			while ((readBytes = dis.read(buffer)) != -1) {
-				fos.write(buffer, 0, readBytes);
-				System.out.println(readBytes);
+			for (String filePath2 : filepath) {
+				Socket socket = serverSocket.accept();
+				
+				InputStream is = socket.getInputStream();
+				DataInputStream dis = new DataInputStream(is);
+				
+				File file = new File(filePath2);
+				fos = new FileOutputStream(file);
+				
+				int readBytes = 0;
+				byte[] buffer = new byte[2048];
+				while ((readBytes = dis.read(buffer)) != -1) {
+					fos.write(buffer, 0, readBytes);
+					System.out.println(readBytes);
+				}
+				
 			}
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-//			e.printStackTrace();
+			e.printStackTrace();
 		} finally {
 			try {
 				fos.close();
